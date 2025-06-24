f06c0733a6f3e4e949942225c6ffd8c0bba51568
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2025-06-23 20:10:36 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import u from '/node_modules/umbrellajs/umbrella.esm.js';
 const selections = u('.selection');
+const seeMores = u('.see-more');
+const seeLesses = u('.see-less');
 const body = u('body');
 const footer = u('footer');
 selections.on('click', (e)=>{
@@ -13,3 +15,13 @@ selections.on('click', (e)=>{
         footer.addClass(selected.data('mode'));
     }, 100)
 })
+seeMores.on('click', (e)=>{
+    u(e.target).parent('.description-text').find('.expanded-description').removeClass('hidden');
+    u(e.target).parent('.description-text').find('.see-less').removeClass('hidden');
+    u(e.target).addClass('hidden');
+});
+seeLesses.on('click', (e)=>{
+    u(e.target).parent('.description-text').find('.expanded-description').addClass('hidden');
+    u(e.target).parent('.description-text').find('.see-more').removeClass('hidden');
+    u(e.target).addClass('hidden');
+});
